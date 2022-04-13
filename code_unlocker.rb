f10fb0d27102378368f376ffdafc788d1da3db4a
scrpd <salamaudavid@gmail.com>
2022-04-13 17:25:24 +0600

    Improve algorithm to find the shortest combinations list

--- code_unlocker.rb
+++ code_unlocker.rb
@@ -28,21 +28,24 @@ module CodeUnlocker
     combinations_list = kwargs[:combinations_list] || [start_combination]
     return pp(*combinations_list) if start_combination == result_combination
 
-    current_number_index = kwargs[:current_number_index] || 0
-    direction = kwargs[:direction] || 'NEXT_NUMBER'
-
     current_combination = start_combination.dup
+    current_number_index = kwargs[:current_number_index] || 0
+
+    short_direction =
+      find_direction(current_combination[current_number_index], result_combination[current_number_index])
+    direction = kwargs[:direction] || short_direction
+
     current_combination[current_number_index] =
       Object.const_get("CodeUnlocker::#{direction}").call(current_combination[current_number_index])
 
     if restricted_combinations.include?(current_combination)
       current_combination[current_number_index] = start_combination[current_number_index]
 
-      options = case direction
-                when 'NEXT_NUMBER'
-                  { current_number_index: current_number_index, direction: 'PREVIOUS_NUMBER' }
-                when 'PREVIOUS_NUMBER'
-                  { current_number_index: NEXT_INDEX.call(current_number_index, current_combination) }
+      options = if direction == short_direction
+                  next_index = NEXT_INDEX.call(current_number_index, current_combination)
+                  { current_number_index: next_index, single_switch: true }
+                else
+                  { current_number_index: current_number_index, direction: opposite_direction(direction) }
                 end
 
       run(
@@ -57,7 +60,9 @@ module CodeUnlocker
     end
 
     combinations_list << current_combination
-    options = if current_combination[current_number_index] == result_combination[current_number_index]
+
+    is_correct_number = current_combination[current_number_index] == result_combination[current_number_index]
+    options = if is_correct_number || kwargs[:single_switch]
                 { current_number_index: NEXT_INDEX.call(current_number_index, current_combination) }
               else
                 { current_number_index: current_number_index, direction: direction }
@@ -74,5 +79,18 @@ module CodeUnlocker
     message = "COMBINATIONS LIST FROM #{combinations_list[0]} TO #{result_combination} IS MISSING." \
               "RESTRICTED COMBINATIONS: #{restricted_combinations}."
     puts(message)
+  end
+
+  def self.find_direction(start_number, final_number)
+    next_numbers = [start_number]
+    (DIGITS.count / 2).times { next_numbers << NEXT_NUMBER.call(next_numbers.last) }
+    next_numbers.include?(final_number) ? 'NEXT_NUMBER' : 'PREVIOUS_NUMBER'
+  end
+
+  def self.opposite_direction(direction)
+    case direction
+    when 'NEXT_NUMBER' then 'PREVIOUS_NUMBER'
+    when 'PREVIOUS_NUMBER' then 'NEXT_NUMBER'
+    end
   end
 end
